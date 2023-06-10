b4432f704e34928b473127471f06a493306f076b
itsora7 <itsora7@gmail.com>
2023-06-10 13:37:10 +1000

    DB

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   try {
     const mongoUrl = "mongodb://127.0.0.1:27017/LMS_api";
+
+    if (!process.env.mongoUrl) {
+      return console.log("Mongo_URL is not defined");
+    }
     const conn = mongoose.connect(mongoUrl);
 
     conn
